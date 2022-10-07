4b475f28ad64eacb727e2ff52cc7983f1d96bd89
Waseem Mira <waseem007_mira@hotmail.com>
2022-10-07 11:48:59 +0400

    watch list back-end complete

--- db/migrate/20221006101816_create_bookmarks.rb
+++ db/migrate/20221006101816_create_bookmarks.rb
@@ -2,8 +2,8 @@ class CreateBookmarks < ActiveRecord::Migration[7.0]
   def change
     create_table :bookmarks do |t|
       t.string :comment
-      t.references :movie
-      t.references :list
+      t.references :movie, foreign_key: true, null: false
+      t.references :list, foreign_key: true, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_101816) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string "comment"
-    t.bigint "movie_id"
-    t.bigint "list_id"
+    t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
@@ -39,4 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_101816) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
 end
